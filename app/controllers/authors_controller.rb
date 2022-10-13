27804c108bfe8ce46b19e425510068126e02042e
Amanda Ross <amikaross@gmail.com>
2022-10-13 08:40:33 -0600

    Add author#new action to controller and new view and spec

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,20 @@ class AuthorsController < ApplicationController
   def show 
     @author = Author.find(params[:id])
   end
+
+  def new 
+  end
+
+  def create
+    author = Author.new({
+      name: params[:author][:name], 
+      dob_year: params[:author][:dob_year], 
+      country: params[:author][:country], 
+      active: params[:author][:active]
+      })
+
+    author.save 
+
+    redirect_to "/authors"
+  end
 end
