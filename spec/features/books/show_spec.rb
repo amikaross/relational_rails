4ed20276f6825cdb08ee7d13f61df09ed1a58fe9
Amanda Ross <amikaross@gmail.com>
2022-10-13 08:41:05 -0600

    Refactor existing test environments

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'the books showpage' do 
   before(:each) do 
-    @author_1 = Author.create!(active: false, dob_year: 1950, name: "Ursula K Leguin", country: "USA")
+    @author_1 = Author.create!(active: false, dob_year: 1950, name: "Nam Nam", country: "USA")
     @author_2 = Author.create!(active: true, dob_year: 1945, name: "Iain Banks", country: "UK")
   
-    @book_1 = @author_1.books.create!(part_of_series: false, word_count: 98000, title: "Left Hand of Darkness", genre: "Scifi/Fantasy")
+    @book_1 = @author_1.books.create!(part_of_series: false, word_count: 98000, title: "Darkness", genre: "Scifi/Fantasy")
     @book_2 = @author_2.books.create!(part_of_series: true, word_count: 56043, title: "Accelerando", genre: "Scifi")
   end
 
