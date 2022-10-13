e571220286aed9d45b909e66df111bde586703d8
Amanda Ross <amikaross@gmail.com>
2022-10-13 13:11:02 -0600

    Refactor create method to use author_params method

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,15 +11,11 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.new({
-      name: params[:author][:name], 
-      dob_year: params[:author][:dob_year], 
-      country: params[:author][:country], 
-      active: params[:author][:active]
-      })
-
-    author.save 
-
+    author = Author.create(author_params)
     redirect_to "/authors"
+  end
+
+  def author_params
+    params.permit(:name, :dob_year, :country, :active)
   end
 end
