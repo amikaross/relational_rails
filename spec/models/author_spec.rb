f49e6f0b4e151ad76a361cf14678df0aa87b076e
Amanda Ross <amikaross@gmail.com>
2022-10-14 08:52:48 -0600

    Update author model to add class method to sort by creation time

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Author do 
-  it {should have_many :books}
+  describe "relationships" do 
+    it {should have_many :books}
+  end
+
+  describe "class methods" do 
+    describe "sort_by_creation" do 
+      it "returns the collection of authors sorted by creation time" do 
+        author_2 = Author.create!(active: false, dob_year: 1950, name: "Nam Nam", country: "USA")
+        author_3 = Author.create!(active: true, dob_year: 1945, name: "Iain Banks", country: "UK")
+        author_1 = Author.create!(active: false, dob_year: 1989, name: "Lee Saville", country: "USA")
+
+        expect(Author.sort_by_creation).to eq([author_2, author_3, author_1])
+      end
+    end
+  end
 end
