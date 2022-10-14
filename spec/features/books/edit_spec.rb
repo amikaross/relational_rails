2f231b8f036de591c8726f3444bd956986aa4047
Amanda Ross <amikaross@gmail.com>
2022-10-14 16:28:40 -0600

    Add links to destroy books on both the books index and author's books index

--- spec/features/books/edit_spec.rb
+++ spec/features/books/edit_spec.rb
@@ -45,5 +45,23 @@ RSpec.describe "edit books" do
         expect(current_path).to eq("/books/#{book_1.id}/edit")
       end
     end
+
+    describe "when I visit the Author's books index page" do 
+      it "has an Update Book button for every book" do 
+        author = Author.create!(active: false, dob_year: 1950, name: "Nam Nam", country: "USA")
+        book_1 = author.books.create!(part_of_series: true, word_count: 98000, title: "Darkness", genre: "Scifi/Fantasy")
+        book_2 = author.books.create!(part_of_series: true, word_count: 23423, title: "Testing a very long name just in case it looks terrible", genre: "Scifi/Fantasy")
+        book_3 = author.books.create!(part_of_series: true, word_count: 3580023, title: "Light", genre: "Scifi/Fantasy")
+        visit "/authors/#{author.id}/books"
+
+        expect(page).to have_button("Update 'Darkness'")
+        expect(page).to have_button("Update 'Testing a very long name just in case it looks terrible'")
+        expect(page).to have_button("Update 'Light'")
+
+        click_button("Update 'Darkness'")
+
+        expect(current_path).to eq("/books/#{book_1.id}/edit")
+      end
+    end
   end
 end
