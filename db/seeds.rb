643575f4610649ed8e0e1631d4101aaef77faae7
Amanda Ross <amikaross@gmail.com>
2022-10-14 18:04:38 -0600

    Refactor views for readibility, no logic or feature changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+author_1 = Author.create(name: "Ursula K Le Guin", dob_year: 1929, country: "United States", active: false)
+author_2 = Author.create(name: "Iain M Banks", dob_year: 1954, country: "Scotland, UK", active: false)
+author_3 = Author.create(name: "N.K. Jemisin", dob_year: 1972, country: "United States", active: true)
+
+author_1.books.create([{title: "A Wizard of Earthsea", genre: "fantasy", word_count: 56533, part_of_series: true}, 
+                       {title: "The Left Hand of Darkness", genre: "scifi", word_count: 89745, part_of_series: true},
+                       {title: "The Disposessed", genre: "scifi", word_count: 115713, part_of_series: true}])
+
+author_2.books.create([{title: "Consider Phlebas", genre: "fantasy", word_count: 174567, part_of_series: true}, 
+                       {title: "The Algebraist", genre: "scifi", word_count: 152359, part_of_series: false},
+                       {title: "Matter", genre: "scifi", word_count: 183775, part_of_series: true}])
+
+author_3.books.create([{title: "The Fifth Season", genre: "fantasy", word_count: 128000, part_of_series: true}, 
+                       {title: "How Long 'til Black Future Month", genre: "non-fiction", word_count: 126359, part_of_series: false},
+                       {title: "The Stone Sky", genre: "scifi", word_count: 124384, part_of_series: true}])
+
+
