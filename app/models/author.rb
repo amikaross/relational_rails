277c552f9087e7c3ef0054a01c2b9ecf03d92ffe
Amanda Ross <amikaross@gmail.com>
2022-10-18 11:53:43 -0400

    Remove 'self' from class and instance methods in models

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,11 +2,11 @@ class Author < ApplicationRecord
   has_many :books
 
   def self.sort_by_creation
-    self.order(:created_at)
+    order(:created_at)
   end
 
   def self.sort_by_book_amount
-    self.left_joins(:books).group(:id).order('COUNT(books.id) DESC')
+    left_joins(:books).group(:id).order('COUNT(books.id) DESC')
   end
 
   def count_books
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,10 +2,10 @@ class Book < ApplicationRecord
   belongs_to :author
 
   def self.part_of_series
-    self.where(part_of_series: true)
+    where(part_of_series: true)
   end
 
   def self.max_word_count(number)
-    self.where("word_count > #{number}")
+    where("word_count > #{number}")
   end
 end
